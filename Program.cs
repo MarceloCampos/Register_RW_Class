ee9acfaaf662d211d52673f4a9bee7d45eb7d292
Marcelo Campos <marcelo.camposs182@gmail.com>
2017-02-17 15:01:17 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*  Random Key 16 long:
+ *  
+ *  IKgyux3Fa4rZo6YD
+    xhoLt4AEMj92xTmC
+    1j4bHnnUDE1TeCKC
+    T3bAWdf7qmSVZmyd
+    KNIW8KTuhmz7Jexv
+    DVk42NBME9LTQr1D
+    iH9uElKgBqe85iYs
+    2DBcP2rZTLXoDp6B
+    P4Y7jM77DPC2zqhD
+    uTnwCBo5ZNWkfyBU
+ * 
+ */
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string register, sub_register, value = string.Empty;
+
+            Register_RW_Class Register_RW = new Register_RW_Class();
+
+            register = "YellowStone";
+            sub_register = "AppKey";
+            value = "PU2MKY";
+
+            Register_RW.Write_Register(register, sub_register, value);
+
+
+            string op = Register_RW.Read_Register(register, sub_register);
+
+            Debug.Print(op);
+        }
+    }
+}
---
+++ Register_RW_Class.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+
+/* by Marcelo Campos
+ * 
+ * Classe para ler e gravar no registro do windows
+ * 
+ * Usa HKEY_CURRENT_USER pois tem acesso direto sem necessidade privilégios administrador
+ * 
+ * REV 0 - Fev17
+ * 
+ */
+
+
+namespace ConsoleApplication1
+{
+    class Register_RW_Class
+    {
+        public string Read_Register(string register, string sub_register)
+        {
+            string rs = string.Empty;
+
+            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(register + "\\" + sub_register);
+
+            rs = (string)registryKey.GetValue(sub_register);
+
+            return rs;
+        }
+
+        public void Write_Register(string register, string sub_register, string value)
+        {
+            RegistryKey key = Registry.CurrentUser.CreateSubKey(register);
+
+            key.CreateSubKey(sub_register);
+            key = key.OpenSubKey(sub_register, true);
+
+            key.SetValue(sub_register, value);
+
+            key.Close();
+
+        }
+
+    }
+
+
+
+
+
+}
